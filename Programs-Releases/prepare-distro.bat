0313a4dcecb4974e67e3a13e9ecdb12a0ca2a782
Patrick Percot <patrick.percot@morbihan.fr>
2015-09-21 18:01:41 +0200

    Batch de préparation des exécutables

---
+++ Programs-Releases/prepare-distro.bat
@@ -0,0 +1,8 @@
+copy ..\BusinessDataController\bin\Release\* .
+copy ..\RngProfileControllerTester\bin\Release\* .
+copy ..\SedaSummaryGeneratorTester\bin\Release\* .
+copy ..\RngProfileControllerTester\bin\Release\* .
+del *.pdb
+del *.vshost.exe.config
+del *.vshost.exe
+del *.vshost.*
