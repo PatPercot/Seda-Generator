0e29bbad6d7bc953204503b1013bd8355a46ace5
PERCOT Patrick <patrick.percot@morbihan.fr>
2017-09-18 16:46:06 +0200

    Dates extrêmes dans les unités documentaires

--- SedaSummaryGeneratorUnitTest/JavaSedaProfileGeneratorCD56_task.sh
+++ SedaSummaryGeneratorUnitTest/JavaSedaProfileGeneratorCD56_task.sh
@@ -11,7 +11,7 @@
 TASK=$1
 
 #GENERATOR : Modifier à chaque changement de version
-JAVASEDAPROFILEGENERATOR=lib-cd56/JavaSedaProfileGenerator-1.1.0.jar
+JAVASEDAPROFILEGENERATOR=lib-cd56/JavaSedaProfileGenerator-1.1.1.jar
 
 #LIB
 ANTLR_LIB=lib-cd56/antlr-2.7.7.jar
